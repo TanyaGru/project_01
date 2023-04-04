78ad8c894d7374c58441934511cb592389264cec
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2023-04-04 13:00:58 +0300

    Выполнено

--- task_2.2.py
+++ task_2.2.py
@@ -39,3 +39,9 @@ def quarter_of():
     pass
 
 quarter_of()
+
+# ох, что то сложно. Можно немного покороче)
+
+def quarter_of(month):
+    q = {1: (1,3), 2:(4,6), 3:(7,9), 4:(10,12)}
+    return [k for k,v in q.items() if v[0] <= month <= v[1]][0]
